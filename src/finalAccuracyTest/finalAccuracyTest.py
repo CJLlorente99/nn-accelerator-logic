Bug that didn't allow proper neuron output calculation solved.

src/finalAccuracyTest/finalAccuracyTest.py
@@ -10,9 +10,9 @@ import numpy as np
 batch_size = 100
 neuronPerLayer = 100
 mod = True  # Change accordingly in modelFilename too
-modelFilename = f'../models/savedModels/MNISTbinNN100Epoch{neuronPerLayer}NPLnllCriterion'
+modelFilename = f'./src/modelCreation/savedModels/MNISTSignbinNN100Epoch{neuronPerLayer}NPLnllCriterion'
 precision = 'bin'
-lastLayerInputsTrainFilename = f'example'
+lastLayerInputsTrainFilename = f'C:/Users/carlo/Desktop/data/inputSimulated/L0'
 lastLayerInputsTestFilename = f'example'
 
 
@@ -55,26 +55,39 @@ model.load_state_dict(torch.load(modelFilename))
 '''
 Load the simulated inputs to the last layer (provided by minimized network)
 '''
-columnTags = [f'N{i}' for i in range(10)]
+# TODO. Change when it is last layer
+columnTags = [f'N{i}' for i in range(100)]
 dfInputsLastLayerTrain = pd.DataFrame(columns=columnTags)
 
+count = 0
 with open(lastLayerInputsTrainFilename) as f:
-        while True:
-            line = np.array(list(f.readline()), dtype=np.float)
-            if not line: break
+    numLines = len(f.readlines())
+    f.seek(0)
+    while True:
+        x = list(f.readline())
+        if len(x):
+            x.pop()
+            line = np.array(x, dtype=np.double)            
+            dfInputsLastLayerTrain = pd.concat([dfInputsLastLayerTrain,
+                                                pd.DataFrame(line, index=columnTags).transpose()])
             
-            dfInputsLastLayerTrain = pd.concat(dfInputsLastLayerTrain,
-                                          pd.DataFrame(line, columns=columnTags))
-
+            count += 1
+            if (count+1) % 5000 == 0:
+                print(f"Load inputs [{count+1:>5d}/{numLines:>5d}]")
+        else:
+            break         
+            
+dfInputsLastLayerTrain[dfInputsLastLayerTrain == 0] = -1
+            
 dfInputsLastLayerTest = pd.DataFrame(columns=columnTags)
         
-with open(lastLayerInputsTestFilename) as f:
-        while True:
-            line = np.array(list(f.readline()), dtype=np.float)
-            if not line: break
+# with open(lastLayerInputsTestFilename) as f:
+#         while True:
+#             line = np.array(list(f.readline()), dtype=np.float)
+#             if not line: break
             
-            dfInputsLastLayerTest = pd.concat(dfInputsLastLayerTest,
-                                          pd.DataFrame(line, columns=columnTags))
+#             dfInputsLastLayerTest = pd.concat(dfInputsLastLayerTest,
+#                                           pd.DataFrame(line, columns=columnTags))
 
 '''
 Test
@@ -89,25 +102,26 @@ size = len(train_dataloader.dataset)
 count = 0
 for index, row in dfInputsLastLayerTrain.iterrows():
     with torch.no_grad():
-        pred = model.forwardLastLayer(row)
-        correct += (pred.argmax(1) == train_dataloader.target[count]).type(torch.float).sum().item()
+        x = np.array([list(row)])
+        pred = model.forwardLastLayer(torch.tensor(x).type(torch.FloatTensor))
+        correct += (pred.argmax(1) == training_data.targets[count].item()).type(torch.float).sum().item()
         count += 1
 
 totalCorrect += correct
 print(f"Train Error: \n Accuracy: {(100 * correct / size):>0.2f}%\n")
     
-print(f'TEST Test\n')
+# print(f'TEST Test\n')
 
-correct = 0
-size = len(test_dataloader.dataset)
-count = 0
-for index, row in dfInputsLastLayerTest.iterrows():
-    with torch.no_grad():
-        pred = model.forwardLastLayer(row)
-        correct += (pred.argmax(1) == test_dataloader.target[count]).type(torch.float).sum().item()
-        count += 1
+# correct = 0
+# size = len(test_dataloader.dataset)
+# count = 0
+# for index, row in dfInputsLastLayerTest.iterrows():
+#     with torch.no_grad():
+#         pred = model.forwardLastLayer(row)
+#         correct += (pred.argmax(1) == test_dataloader.target[count]).type(torch.float).sum().item()
+#         count += 1
 
-totalCorrect += correct
-print(f"Test Error: \n Accuracy: {(100 * correct / size):>0.2f}%\n")
+# totalCorrect += correct
+# print(f"Test Error: \n Accuracy: {(100 * correct / size):>0.2f}%\n")
 
 print(f"Total Test Error: \n Accuracy: {(100 * totalCorrect / sizeTotal):>0.2f}%\n")

src/importanceCalculation/gradientCalculation.py
@@ -8,7 +8,7 @@ from ttUtilities.helpLayerNeuronGenerator import HelpGenerator
 
 neuronPerLayer = 100
 modelFilename = f'src\modelCreation\savedModels\MNISTSignbinNN100Epoch100NPLnllCriterion'
-batch_size = 64
+batch_size = 1
 perGradientSampling = 1
 # Check mps maybe if working in MacOS
 device = 'cuda' if torch.cuda.is_available() else 'cpu'

src/importanceCalculation/importanceVariousThrehold.py
@@ -8,7 +8,7 @@ from modules.binaryEnergyEfficiency import BinaryNeuralNetwork
 from ttUtilities.helpLayerNeuronGenerator import HelpGenerator
 import numpy as np
 
-neuronPerLayer = 300
+neuronPerLayer = 500
 modelFilename = f'src\modelCreation\savedModels\MNISTSignbinNN100Epoch{neuronPerLayer}NPLnllCriterion'
 batch_size = 64
 perGradientSampling = 1
@@ -70,7 +70,7 @@ Calculate importance per class per neuron
 # Compute importance
 
 importance = model.computeImportance(neuronPerLayer)
-importanceThresholds = np.logspace(-3, -1, num=3, base=10)
+importanceThresholds = np.logspace(-5, -1, num=5, base=10)
 
 for threshold in importanceThresholds:
 
@@ -86,7 +86,7 @@ for threshold in importanceThresholds:
     # Plot importance of neurons per layer
 
     for i in range(len(accLayers) - 1):
-        # accLayers[i].plotImportancePerNeuron(f'Threshold {threshold:.0e}', True)
+        accLayers[i].plotImportancePerNeuron(f'Threshold {threshold:.0e}', True)
         # accLayers[i].plotImportancePerClass(f'Layer {i} Threshold {threshold:.0e}', True)
         accLayers[i].plotNumImportantClasses(f'NPL {neuronPerLayer} Layer {i} Threshold {threshold:.0e}', True, save=True)
         # accLayers[i].saveImportance(f'data/layersImportance/layer{i}Importance{threshold:.0e}GradientBinarySignBNN50epochs{neuronPerLayer}npl')