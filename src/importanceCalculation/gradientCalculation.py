Calculation of importance for vgg fixed

src/importanceCalculation/gradientCalculation.py
@@ -3,10 +3,13 @@ from modelsCommon.auxTransformations import ToBlackAndWhite, ToSign
 from torchvision import datasets
 from torchvision.transforms import ToTensor, Compose, Normalize, RandomHorizontalFlip, RandomCrop
 from torch.utils.data import DataLoader
-from modules.binaryVGGSmall import VGGSmall
+from modules.vggSmall import VGGSmall
+import plotly.graph_objects as go
+import numpy as np
+import pandas as pd
 
-modelFilename = f'src\modelCreation\savedModels\MNISTSignbinNN50Epoch4096NPLhingeCriterion'
-batch_size = 1
+modelFilename = f'src\modelCreation\savedModels\VGGSmall'
+batch_size = 64
 perGradientSampling = 1
 # Check mps maybe if working in MacOS
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -16,11 +19,11 @@ Importing CIFAR10 dataset
 '''
 print(f'DOWNLOAD DATASET\n')
 train_dataset = datasets.CIFAR10(root='./data', train=True, transform=Compose([
-    RandomHorizontalFlip(),
-    RandomCrop(32, 4),
-    ToTensor(),
-    Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))]),
-                                 download=False)
+	RandomHorizontalFlip(),
+	RandomCrop(32, 4),
+	ToTensor(),
+	Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))]),
+								 download=False)
 
 '''
 Create DataLoader
@@ -46,15 +49,98 @@ model.registerHooks()
 model.eval()
 
 for i in range(sampleSize):
-    X, y = train_dataloader.dataset[i]
-    model.zero_grad()
-    pred = model(X)
-    pred[0, y].backward()
+	X, y = train_dataloader.dataset[i]
+	model.zero_grad()
+	pred = model(X)
+	pred[0, y].backward()
 
-    if (i+1) % 500 == 0:
-        print(f"Get Gradients and Activation Values [{i+1:>5d}/{sampleSize:>5d}]")
+	if (i+1) % 500 == 0:
+		print(f"Get Gradients and Activation Values [{i+1:>5d}/{sampleSize:>5d}]")
 
 model.listToArray()  # Hopefully improves memory usage
-model.saveActivations(f'data/activations/vggSmall')
-model.saveGradients(f'data/gradients/vggSmall')
+
+importances = model.computeImportance()
+# model.saveActivations(f'data/activations/vggSmall')
+# model.saveGradients(f'data/gradients/vggSmall')
+
+'''
+Calculate importance scores
+'''
+threshold = 10e-50
+for i in range(len(importances)):
+	importances[i] = (importances[i] > threshold)
+
+'''
+Calculate importance per class
+'''
+importancePerClass = {}
+# Initialize for 10 classes and layers
+for grad in model.helpHookList:
+	importancePerClass[grad] = {}
+	for i in range(10):
+		importancePerClass[grad][i] = []
+
+# Iterate through the calculated importances and assign depending on the class
+imp = 0
+for grad in model.helpHookList:
+	importance = importances[imp]
+	if importance.ndim > 2:
+		for i in range(importance.shape[1]):
+			importancePerClass[grad][train_dataset.targets[i]].append(importance[:, i, :])
+	else:
+		for i in range(importance.shape[0]):
+			importancePerClass[grad][train_dataset.targets[i]].append(importance[i, :])
+	imp += 1
+ 
+ # Change from list to array each entry of importancePerClass
+for grad in model.helpHookList:
+	for nClass in importancePerClass[grad]:
+		importancePerClass[grad][nClass] = np.array(importancePerClass[grad][nClass])
+ 
+ # Iterate through the importance scores and convert them into importance values
+for grad in model.helpHookList:
+	for nClass in importancePerClass[grad]:
+		if importancePerClass[grad][nClass].ndim > 2: # Then it comes from a conv layer
+			importancePerClass[grad][nClass] = importancePerClass[grad][nClass].sum(0) / len(importancePerClass[grad][nClass])
+			# Take the max score
+			importancePerClass[grad][nClass] = importancePerClass[grad][nClass].max(0)
+		else: # Then it comes from a neural layer
+			importancePerClass[grad][nClass] = importancePerClass[grad][nClass].sum(0) / len(importancePerClass[grad][nClass])
+	
+# Join the lists so each row is a class and each column a filter/neuron
+for grad in importancePerClass:
+	importancePerClass[grad] = np.row_stack(tuple(importancePerClass[grad].values()))
+	
+'''
+Print results
+'''
+for grad in importancePerClass:
+    # Print aggregated importance
+	aux = importancePerClass[grad].sum(0)
+	aux.sort()	
+ 
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(y=aux))
+	fig.update_layout(title=f'{grad} total importance')
+	fig.show()
+	
+	# Print classes that are important
+	aux = (importancePerClass[grad] > 0).sum(0)
+	aux.sort()
+	
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(y=aux))
+	fig.update_layout(title=f'{grad} number of important classes')
+	fig.show()
+ 
+ 	# Print importance per class
+	aux = pd.DataFrame(importancePerClass[grad],
+                    columns=[f'filter{i}' for i in range(importancePerClass[grad].shape[1])])
+	fig = go.Figure()
+	for index, row in aux.iterrows():
+		fig.add_trace(go.Bar(name=f'class{index}', y=row, x=aux.columns))
+	fig.update_layout(title=f'{grad} importance per class', barmode='stack')
+	fig.show()
+	pass
+
 

src/importanceCalculation/modules/binaryVGGSmall.py
@@ -15,60 +15,60 @@ class VGGSmall(nn.Module):
 
 		# Layer 0
 		self.conv0 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
-		self.relu0 = nn.ReLU(inplace=True)
+		self.ste0 = STEFunction()
 		self.maxpool0 = nn.MaxPool2d(kernel_size=2, stride=2)
-		self.helpHookList.append('relu0')
+		self.helpHookList.append('ste0')
 
 		# Layer 1
 		self.conv1 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-		self.relu1 = nn.ReLU(inplace=True)
+		self.ste1 = STEFunction()
 		self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-		self.helpHookList.append('relu1')
+		self.helpHookList.append('ste1')
   
 		# Layer 2.1
 		self.conv21 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
-		self.relu21 = nn.ReLU(inplace=True)
-		self.helpHookList.append('relu21')
+		self.ste21 = STEFunction()
+		self.helpHookList.append('ste21')
   
 		# Layer 2.2
 		self.conv22 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
-		self.relu22 = nn.ReLU(inplace=True)
+		self.ste22 = STEFunction()
 		self.maxpool22 = nn.MaxPool2d(kernel_size=2, stride=2)
-		self.helpHookList.append('relu22')
+		self.helpHookList.append('ste22')
   
 		# Layer 3.1
 		self.conv31 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
-		self.relu31 = nn.ReLU(inplace=True)
-		self.helpHookList.append('relu31')
+		self.ste31 = STEFunction()
+		self.helpHookList.append('ste31')
 
 		# Layer 3.2
 		self.conv32 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-		self.relu32 = nn.ReLU(inplace=True)
+		self.ste32 = STEFunction()
 		self.maxpool32 = nn.MaxPool2d(kernel_size=2, stride=2)
-		self.helpHookList.append('relu32')
+		self.helpHookList.append('ste32')
   
 		# Layer 4.1
 		self.conv41 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-		self.relu41 = nn.ReLU(inplace=True)
-		self.helpHookList.append('relu41')
+		self.ste41 = STEFunction()
+		self.helpHookList.append('ste41')
   
 		# Layer 4.2
 		self.conv42 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-		self.relu42 = nn.ReLU(inplace=True)
+		self.ste42 = STEFunction()
 		self.maxpool42 = nn.MaxPool2d(kernel_size=2, stride=2)
-		self.helpHookList.append('relu42')
+		self.helpHookList.append('ste42')
   
 		# Layer FC0
 		self.dropoutl0 = nn.Dropout(0.5)
 		self.l0 = nn.Linear(512, 512)
-		self.relul0 = nn.ReLU(inplace=True)
-		self.helpHookList.append('relul0')
+		self.stel0 = STEFunction()
+		self.helpHookList.append('stel0')
   
 		# Layer FC1
 		self.dropoutl1 = nn.Dropout(0.5)
 		self.l1 = nn.Linear(512, 512)
-		self.relul1 = nn.ReLU(inplace=True)
-		self.helpHookList.append('relul1')
+		self.stel1 = STEFunction()
+		self.helpHookList.append('stel1')
   
 		# Layer FC2
 		self.l2 = nn.Linear(512, 10)
@@ -93,50 +93,52 @@ class VGGSmall(nn.Module):
 	def forward(self, x):
 		# Layer 0
 		x = self.conv0(x)
-		x = self.relu0(x)
+		x = self.ste0(x)
 		x = self.maxpool0(x)
 
 		# Layer 1
 		x = self.conv1(x)
-		x = self.relu1(x)
+		x = self.ste1(x)
 		x = self.maxpool1(x)
   
 		# Layer 2.1
 		x = self.conv21(x)
-		x = self.relu21(x)
+		x = self.ste21(x)
   
 		# Layer 2.2
 		x = self.conv22(x)
-		x = self.relu22(x)
+		x = self.ste22(x)
 		x = self.maxpool22(x)
   
 		# Layer 3.1
 		x = self.conv31(x)
-		x = self.relu31(x)
+		x = self.ste31(x)
 
 		# Layer 3.2
 		x = self.conv32(x)
-		x = self.relu32(x)
+		x = self.ste32(x)
 		x = self.maxpool32(x)
   
 		# Layer 4.1
 		x = self.conv41(x)
-		x = self.relu41(x)
+		x = self.ste41(x)
   
 		# Layer 4.2
 		x = self.conv42(x)
-		x = self.relu42(x)
+		x = self.ste42(x)
 		x = self.maxpool42(x)
+  
+		x = x.reshape(x.size(0), -1)
   
 		# Layer FC0
 		x = self.dropoutl0(x)
 		x = self.l0(x)
-		x = self.relul0(x)
+		x = self.stel0(x)
   
 		# Layer FC1
 		x = self.dropoutl1(x)
 		x = self.l1(x)
-		x = self.relul1(x)
+		x = self.stel1(x)
   
 		# Layer FC2
 		x = self.l2(x)
@@ -146,90 +148,90 @@ class VGGSmall(nn.Module):
 	# Probably there exists a better way to do this
 	def registerHooks(self):
 		# Forward hooks are needed to compute importance
-		self.relu0.register_forward_hook(self.forward_hook_relu0)
-		self.relu1.register_forward_hook(self.forward_hook_relu1)
-		self.relu21.register_forward_hook(self.forward_hook_relu21)
-		self.relu22.register_forward_hook(self.forward_hook_relu22)
-		self.relu31.register_forward_hook(self.forward_hook_relu31)
-		self.relu32.register_forward_hook(self.forward_hook_relu32)
-		self.relu41.register_forward_hook(self.forward_hook_relu41)
-		self.relu42.register_forward_hook(self.forward_hook_relu42)
-		self.relul0.register_forward_hook(self.forward_hook_relul0)
-		self.relul1.register_forward_hook(self.forward_hook_relul1)
+		self.ste0.register_forward_hook(self.forward_hook_ste0)
+		self.ste1.register_forward_hook(self.forward_hook_ste1)
+		self.ste21.register_forward_hook(self.forward_hook_ste21)
+		self.ste22.register_forward_hook(self.forward_hook_ste22)
+		self.ste31.register_forward_hook(self.forward_hook_ste31)
+		self.ste32.register_forward_hook(self.forward_hook_ste32)
+		self.ste41.register_forward_hook(self.forward_hook_ste41)
+		self.ste42.register_forward_hook(self.forward_hook_ste42)
+		self.stel0.register_forward_hook(self.forward_hook_stel0)
+		self.stel1.register_forward_hook(self.forward_hook_stel1)
   
 		# Backward hooks are needed to compute importance
-		self.relu0.register_full_backward_hook(self.backward_hook_relu0)
-		self.relu1.register_full_backward_hook(self.backward_hook_relu1)
-		self.relu21.register_full_backward_hook(self.backward_hook_relu21)
-		self.relu22.register_full_backward_hook(self.backward_hook_relu22)
-		self.relu31.register_full_backward_hook(self.backward_hook_relu31)
-		self.relu32.register_full_backward_hook(self.backward_hook_relu32)
-		self.relu41.register_full_backward_hook(self.backward_hook_relu41)
-		self.relu42.register_full_backward_hook(self.backward_hook_relu42)
-		self.relul0.register_full_backward_hook(self.backward_hook_relul0)
-		self.relul1.register_full_backward_hook(self.backward_hook_relul1)
+		self.ste0.register_full_backward_hook(self.backward_hook_ste0)
+		self.ste1.register_full_backward_hook(self.backward_hook_ste1)
+		self.ste21.register_full_backward_hook(self.backward_hook_ste21)
+		self.ste22.register_full_backward_hook(self.backward_hook_ste22)
+		self.ste31.register_full_backward_hook(self.backward_hook_ste31)
+		self.ste32.register_full_backward_hook(self.backward_hook_ste32)
+		self.ste41.register_full_backward_hook(self.backward_hook_ste41)
+		self.ste42.register_full_backward_hook(self.backward_hook_ste42)
+		self.stel0.register_full_backward_hook(self.backward_hook_stel0)
+		self.stel1.register_full_backward_hook(self.backward_hook_stel1)
 
 	# Define all backward hooks
-	def backward_hook_relu0(self, module, grad_input, grad_output):
-		self.gradsFromHooks['relu0']['backward'].append(grad_output[0].cpu().detach().numpy()[0])
+	def backward_hook_ste0(self, module, grad_input, grad_output):
+		self.gradsFromHooks['ste0']['backward'].append(grad_output[0].cpu().detach().numpy()[0])
   
-	def backward_hook_relu1(self, module, grad_input, grad_output):
-		self.gradsFromHooks['relu1']['backward'].append(grad_output[0].cpu().detach().numpy()[0])
+	def backward_hook_ste1(self, module, grad_input, grad_output):
+		self.gradsFromHooks['ste1']['backward'].append(grad_output[0].cpu().detach().numpy()[0])
   
-	def backward_hook_relu21(self, module, grad_input, grad_output):
-		self.gradsFromHooks['relu21']['backward'].append(grad_output[0].cpu().detach().numpy()[0])
+	def backward_hook_ste21(self, module, grad_input, grad_output):
+		self.gradsFromHooks['ste21']['backward'].append(grad_output[0].cpu().detach().numpy()[0])
   
-	def backward_hook_relu22(self, module, grad_input, grad_output):
-		self.gradsFromHooks['relu22']['backward'].append(grad_output[0].cpu().detach().numpy()[0])
+	def backward_hook_ste22(self, module, grad_input, grad_output):
+		self.gradsFromHooks['ste22']['backward'].append(grad_output[0].cpu().detach().numpy()[0])
   
-	def backward_hook_relu31(self, module, grad_input, grad_output):
-		self.gradsFromHooks['relu31']['backward'].append(grad_output[0].cpu().detach().numpy()[0])
+	def backward_hook_ste31(self, module, grad_input, grad_output):
+		self.gradsFromHooks['ste31']['backward'].append(grad_output[0].cpu().detach().numpy()[0])
   
-	def backward_hook_relu32(self, module, grad_input, grad_output):
-		self.gradsFromHooks['relu32']['backward'].append(grad_output[0].cpu().detach().numpy()[0])
+	def backward_hook_ste32(self, module, grad_input, grad_output):
+		self.gradsFromHooks['ste32']['backward'].append(grad_output[0].cpu().detach().numpy()[0])
   
-	def backward_hook_relu41(self, module, grad_input, grad_output):
-		self.gradsFromHooks['relu41']['backward'].append(grad_output[0].cpu().detach().numpy()[0])
+	def backward_hook_ste41(self, module, grad_input, grad_output):
+		self.gradsFromHooks['ste41']['backward'].append(grad_output[0].cpu().detach().numpy()[0])
   
-	def backward_hook_relu42(self, module, grad_input, grad_output):
-		self.gradsFromHooks['relu42']['backward'].append(grad_output[0].cpu().detach().numpy()[0])
+	def backward_hook_ste42(self, module, grad_input, grad_output):
+		self.gradsFromHooks['ste42']['backward'].append(grad_output[0].cpu().detach().numpy()[0])
   
-	def backward_hook_relul0(self, module, grad_input, grad_output):
-		self.gradsFromHooks['relul0']['backward'].append(grad_output[0].cpu().detach().numpy()[0])
+	def backward_hook_stel0(self, module, grad_input, grad_output):
+		self.gradsFromHooks['stel0']['backward'].append(grad_output[0].cpu().detach().numpy()[0])
   
-	def backward_hook_relul1(self, module, grad_input, grad_output):
-		self.gradsFromHooks['relul1']['backward'].append(grad_output[0].cpu().detach().numpy()[0])
+	def backward_hook_stel1(self, module, grad_input, grad_output):
+		self.gradsFromHooks['stel1']['backward'].append(grad_output[0].cpu().detach().numpy()[0])
   
 	# Define all forward hooks
-	def backward_hook_relu0(self, module, val_input, val_output):
-		self.activationsFromHooks['relu0']['forward'].append(val_output[0].cpu().detach().numpy()[0])
+	def backward_hook_ste0(self, module, val_input, val_output):
+		self.activationsFromHooks['ste0']['forward'].append(val_output[0].cpu().detach().numpy()[0])
   
-	def backward_hook_relu1(self, module, val_input, val_output):
-		self.activationsFromHooks['relu1']['forward'].append(val_output[0].cpu().detach().numpy()[0])
+	def backward_hook_ste1(self, module, val_input, val_output):
+		self.activationsFromHooks['ste1']['forward'].append(val_output[0].cpu().detach().numpy()[0])
   
-	def backward_hook_relu21(self, module, val_input, val_output):
-		self.activationsFromHooks['relu21']['forward'].append(val_output[0].cpu().detach().numpy()[0])
+	def backward_hook_ste21(self, module, val_input, val_output):
+		self.activationsFromHooks['ste21']['forward'].append(val_output[0].cpu().detach().numpy()[0])
   
-	def backward_hook_relu22(self, module, val_input, val_output):
-		self.activationsFromHooks['relu22']['forward'].append(val_output[0].cpu().detach().numpy()[0])
+	def backward_hook_ste22(self, module, val_input, val_output):
+		self.activationsFromHooks['ste22']['forward'].append(val_output[0].cpu().detach().numpy()[0])
   
-	def backward_hook_relu31(self, module, val_input, val_output):
-		self.activationsFromHooks['relu31']['forward'].append(val_output[0].cpu().detach().numpy()[0])
+	def backward_hook_ste31(self, module, val_input, val_output):
+		self.activationsFromHooks['ste31']['forward'].append(val_output[0].cpu().detach().numpy()[0])
   
-	def backward_hook_relu32(self, module, val_input, val_output):
-		self.activationsFromHooks['relu32']['forward'].append(val_output[0].cpu().detach().numpy()[0])
+	def backward_hook_ste32(self, module, val_input, val_output):
+		self.activationsFromHooks['ste32']['forward'].append(val_output[0].cpu().detach().numpy()[0])
   
-	def backward_hook_relu41(self, module, val_input, val_output):
-		self.activationsFromHooks['relu41']['forward'].append(val_output[0].cpu().detach().numpy()[0])
+	def backward_hook_ste41(self, module, val_input, val_output):
+		self.activationsFromHooks['ste41']['forward'].append(val_output[0].cpu().detach().numpy()[0])
   
-	def backward_hook_relu42(self, module, val_input, val_output):
-		self.activationsFromHooks['relu42']['forward'].append(val_output[0].cpu().detach().numpy()[0])
+	def backward_hook_ste42(self, module, val_input, val_output):
+		self.activationsFromHooks['ste42']['forward'].append(val_output[0].cpu().detach().numpy()[0])
   
-	def backward_hook_relul0(self, module, val_input, val_output):
-		self.activationsFromHooks['relul0']['forward'].append(val_output[0].cpu().detach().numpy()[0])
+	def backward_hook_stel0(self, module, val_input, val_output):
+		self.activationsFromHooks['stel0']['forward'].append(val_output[0].cpu().detach().numpy()[0])
   
-	def backward_hook_relul1(self, module, val_input, val_output):
-		self.activationsFromHooks['relul1']['forward'].append(val_output[0].cpu().detach().numpy()[0])
+	def backward_hook_stel1(self, module, val_input, val_output):
+		self.activationsFromHooks['stel1']['forward'].append(val_output[0].cpu().detach().numpy()[0])
   
 	# Change each hook list to an equivalent array
 	def listToArray(self):

src/modelCreation/modules/vggSmall.py
@@ -13,50 +13,50 @@ class VGGSmall(nn.Module):
 
 		# Layer 0
 		self.conv0 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
-		self.ste0 = STEFunction()
+		self.relu0 = nn.ReLU()
 		self.maxpool0 = nn.MaxPool2d(kernel_size=2, stride=2)
 
 		# Layer 1
 		self.conv1 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-		self.ste1 = STEFunction()
+		self.relu1 = nn.ReLU()
 		self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
   
 		# Layer 2.1
 		self.conv21 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
-		self.ste21 = nn.ReLU()
+		self.relu21 = nn.ReLU()
   
 		# Layer 2.2
 		self.conv22 = nn.Conv2d(256, 256, kernel_size=3, padding=1)
-		self.ste22 = STEFunction()
+		self.relu22 = nn.ReLU()
 		self.maxpool22 = nn.MaxPool2d(kernel_size=2, stride=2)
   
 		# Layer 3.1
 		self.conv31 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
-		self.ste31 = nn.ReLU()
+		self.relu31 = nn.ReLU()
 
 		# Layer 3.2
 		self.conv32 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-		self.ste32 = STEFunction()
+		self.relu32 = nn.ReLU()
 		self.maxpool32 = nn.MaxPool2d(kernel_size=2, stride=2)
   
 		# Layer 4.1
 		self.conv41 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-		self.ste41 = nn.ReLU()
+		self.relu41 = nn.ReLU()
   
 		# Layer 4.2
 		self.conv42 = nn.Conv2d(512, 512, kernel_size=3, padding=1)
-		self.ste42 = STEFunction()
+		self.relu42 = nn.ReLU()
 		self.maxpool42 = nn.MaxPool2d(kernel_size=2, stride=2)
   
 		# Layer FC0
 		self.dropoutl0 = nn.Dropout(0.5)
 		self.l0 = nn.Linear(512, 512)
-		self.stel0 = STEFunction()
+		self.relul0 = nn.ReLU()
   
 		# Layer FC1
 		self.dropoutl1 = nn.Dropout(0.5)
 		self.l1 = nn.Linear(512, 512)
-		self.stel1 = STEFunction()
+		self.relul1 = nn.ReLU()
   
 		# Layer FC2
 		self.l2 = nn.Linear(512, 10)
@@ -72,39 +72,39 @@ class VGGSmall(nn.Module):
 	def forward(self, x):
 		# Layer 0
 		x = self.conv0(x)
-		x = self.ste0(x)
+		x = self.relu0(x)
 		x = self.maxpool0(x)
 
 		# Layer 1
 		x = self.conv1(x)
-		x = self.ste1(x)
+		x = self.relu1(x)
 		x = self.maxpool1(x)
   
 		# Layer 2.1
 		x = self.conv21(x)
-		x = self.ste21(x)
+		x = self.relu21(x)
   
 		# Layer 2.2
 		x = self.conv22(x)
-		x = self.ste22(x)
+		x = self.relu22(x)
 		x = self.maxpool22(x)
   
 		# Layer 3.1
 		x = self.conv31(x)
-		x = self.ste31(x)
+		x = self.relu31(x)
 
 		# Layer 3.2
 		x = self.conv32(x)
-		x = self.ste32(x)
+		x = self.relu32(x)
 		x = self.maxpool32(x)
   
 		# Layer 4.1
 		x = self.conv41(x)
-		x = self.ste41(x)
+		x = self.relu41(x)
   
 		# Layer 4.2
 		x = self.conv42(x)
-		x = self.ste42(x)
+		x = self.relu42(x)
 		x = self.maxpool42(x)
   
 		x = x.reshape(x.size(0), -1)
@@ -112,12 +112,12 @@ class VGGSmall(nn.Module):
 		# Layer FC0
 		x = self.dropoutl0(x)
 		x = self.l0(x)
-		x = self.stel0(x)
+		x = self.relul0(x)
   
 		# Layer FC1
 		x = self.dropoutl1(x)
 		x = self.l1(x)
-		x = self.stel1(x)
+		x = self.relul1(x)
   
 		# Layer FC2
 		x = self.l2(x)

src/modelCreation/trainAndSaveModelVGG.py
@@ -4,7 +4,7 @@ from modelsCommon.auxTransformations import *
 from torchvision import datasets
 from torchvision.transforms import ToTensor, Compose, Normalize, RandomHorizontalFlip, RandomCrop
 from torch.utils.data import DataLoader
-from modules.binaryVGGSmall import VGGSmall
+from modules.binaryVggSmall import VGGSmall
 import torch.optim as optim
 import torch.nn as nn
 
@@ -57,4 +57,4 @@ trainAndTest(epochs, train_dataloader, test_dataloader, model, opt, criterion)
 Save
 '''
 
-torch.save(model.state_dict(), f'./src/modelCreation/savedModels/VGGSmall')
+torch.save(model.state_dict(), f'./src/modelCreation/savedModels/binaryVGGSmall')