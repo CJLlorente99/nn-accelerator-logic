20bdd4734d3d05cd1c53ad04ba05c3b09f2d7394
CJLlorente99 <carlosllo@hotmail.com>
2023-06-19 11:52:58 +0200

    New binary very small with one more fc layer (closer to actual vggsmall model). train and save turned into script

--- src/modelCreation/modelsCommon/auxFunc.py
+++ src/modelCreation/modelsCommon/auxFunc.py
@@ -55,6 +55,34 @@ def test(dataloader, dataloader2, model, criterion):
 	correct /= sizeTotal
 	print(f"Test Error Total: \n Accuracy: {(100*correct):>0.2f}%, Avg loss: {test_loss:>8f} \n")
 
+def testReturn(dataloader, dataloader2, model, criterion):
+	sizeTotal = len(dataloader.dataset) + len(dataloader2.dataset)
+	size = len(dataloader.dataset)
+	num_batchesTotal = len(dataloader) + len(dataloader2)
+	num_batches = len(dataloader)
+	model.eval()
+	test_loss, correct = 0, 0
+	res = []
+	with torch.no_grad():
+		for X, y in dataloader:
+			X, y = X.to(device), y.to(device)
+			pred = model(X)
+			test_loss += criterion(pred, y).item()
+			correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+
+		res.append(f"Test Error Test: \n Accuracy: {(100 * correct / size):>0.2f}%, Avg loss: {test_loss / num_batches:>8f} \n")
+
+		for X, y in dataloader2:
+			X, y = X.to(device), y.to(device)
+			pred = model(X)
+			test_loss += criterion(pred, y).item()
+			correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+	test_loss /= num_batchesTotal
+	correct /= sizeTotal
+	res.append(f"Test Error Total: \n Accuracy: {(100*correct):>0.2f}%, Avg loss: {test_loss:>8f} \n")
+
+	return res
+
 
 def trainAndTest(epochs, train_dataloader, test_dataloader, model, opt, criterion):
 	for t in range(epochs):
