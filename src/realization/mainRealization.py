7c06dd0430201901e2179b8f52f2c0309eee97c6
CJLlorente99 <cj.llorente@alumnos.upm.es>
2023-07-12 11:45:19 +0200

    Realization for all optimization modes

--- src/realization/mainRealization.py
+++ src/realization/mainRealization.py
@@ -1,10 +1,40 @@
 from ttUtilities.isfRealization import DNFRealization
 
 modelName = 'eeb_100ep_100npl'
-ttFolderName = f'/media/carlosl/CHAR/data/layersTT/{modelName}'
+dataFolder = 'data'
+
+'''
+Not optimized
+'''
+print('CREATING NOT OPTIMIZED PLA FILES')
+ttFolderName = f'{dataFolder}/layersTT/notOptimized/{modelName}'
 
 # Create DNFRealization object
 dnf = DNFRealization(ttFolderName)
 
-# Create PLA file for ABC
-dnf.createPLAFileABC(f'/media/carlosl/CHAR/data/plas/{modelName}/ABC')
+# Create PLA file for ABC (not Optimized)
+dnf.createPLAFileABC(f'{dataFolder}/plas/{modelName}/ABCNotOptimized')
+
+'''
+Optimized per entry
+'''
+print('CREATING OPTIMIZED PER ENTRY PLA FILES')
+ttFolderName = f'{dataFolder}/layersTT/optimizedPerEntry/{modelName}'
+
+# Create DNFRealization object
+dnf = DNFRealization(ttFolderName)
+
+# Create PLA file for ABC (not Optimized)
+dnf.createPLAFileABC(f'{dataFolder}/plas/{modelName}/ABCOptimizedPerEntry')
+
+'''
+Optimized per class
+'''
+print('CREATING OPTIMIZED PER CLASS PLA FILES')
+ttFolderName = f'{dataFolder}/layersTT/notOptimized/{modelName}'
+
+# Create DNFRealization object
+dnf = DNFRealization(ttFolderName)
+
+# Create PLA file for ABC (not Optimized)
+dnf.createPLAFileABC(f'{dataFolder}/plas/{modelName}/ABCNotOptimizedPerClass')
